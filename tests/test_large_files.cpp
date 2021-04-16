ec8ae21cbc506bd2f4550c175e84fe886738e6e9
Jacob Lewallen <jlewallen@gmail.com>
2021-04-16 13:36:18 -0700

    feat: Add record index to large file test.

--- tests/test_large_files.cpp
+++ tests/test_large_files.cpp
@@ -57,13 +57,18 @@ TYPED_TEST(LargeFileFixture, WriteOneMegabyteIndexed) {
         ASSERT_EQ(chain.find("data.txt", open_file_config{ }), 1);
         file_appender opened{ chain, &chain, chain.open() };
 
-        auto tree_sector = memory.allocator().allocate();
-        typename TypeParam::second_type tree{ memory.buffers(), memory.sectors(), memory.allocator(), tree_sector, "tree" };
+        auto position_index_sector = memory.allocator().allocate();
+        typename TypeParam::second_type position_index{ memory.buffers(), memory.sectors(), memory.allocator(), position_index_sector, "posidx" };
 
-        ASSERT_EQ(tree.create(), 0);
+        auto record_index_sector = memory.allocator().allocate();
+        typename TypeParam::second_type record_index{ memory.buffers(), memory.sectors(), memory.allocator(), record_index_sector, "recidx" };
+
+        ASSERT_EQ(position_index.create(), 0);
+        ASSERT_EQ(record_index.create(), 0);
 
         auto written = 0u;
         auto cursor = opened.cursor();
+        auto record_number = 0u;
         {
             phydebugf("suppressing 1MB of writes from debug");
             suppress_logs sl;
@@ -73,17 +78,20 @@ TYPED_TEST(LargeFileFixture, WriteOneMegabyteIndexed) {
                     if (written == 0 || opened.cursor().sector != cursor.sector) {
                         cursor = opened.cursor();
 
-                        // log_configure_level(LogLevels::DEBUG);
-                        phydebugf("cursor sector=%d position=%d position_atsos=%d", cursor.sector, cursor.position, cursor.position_at_start_of_sector);
-                        // log_configure_level(LogLevels::NONE);
+                        log_configure_level(LogLevels::DEBUG);
+                        phydebugf("cursor sector=%d position=%d position_atsos=%d record_number=%d",
+                                  cursor.sector, cursor.position, cursor.position_at_start_of_sector, record_number);
+                        log_configure_level(LogLevels::NONE);
 
-                        ASSERT_EQ(tree.add(cursor.position_at_start_of_sector, cursor.sector), 0);
+                        ASSERT_EQ(position_index.add(cursor.position_at_start_of_sector, cursor.sector), 0);
+                        ASSERT_EQ(record_index.add(record_number, cursor.position), 0);
                     }
                 }
 
                 auto wrote = opened.write(lorem1k);
                 ASSERT_GT(wrote, 0);
                 written += wrote;
+                record_number++;
             }
             ASSERT_EQ(opened.flush(), 0);
         }
@@ -91,11 +99,11 @@ TYPED_TEST(LargeFileFixture, WriteOneMegabyteIndexed) {
         uint32_t end = UINT32_MAX;
         uint32_t found_key = 0;
         uint32_t found_value = 0;
-        ASSERT_EQ(tree.find_last_less_then(end, &found_value, &found_key), 1);
+        ASSERT_EQ(position_index.find_last_less_then(end, &found_value, &found_key), 1);
 
         ASSERT_EQ(found_key, cursor.position_at_start_of_sector);
         ASSERT_EQ(found_value, cursor.sector);
 
-        tree.log();
+        position_index.log();
     });
 }
